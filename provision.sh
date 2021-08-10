8739fb9cf29c7686b55eee353e130cfedb48e2f8
twilliams9397 <twilliams@spartaglobal.com>
2021-08-10 10:22:17 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -24,3 +24,9 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update -y
 
 sudo apt-get install ansible
+
+sudo echo "[web]
+192.168.33.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant
+
+[db]
+192.168.33.11 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/hosts/

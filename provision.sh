09a44bfc20af1aecd44aed94520def84fdd3cbd3
twilliams9397 <twilliams@spartaglobal.com>
2021-08-10 10:17:10 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -2,13 +2,25 @@
 
 # provisioning file for the ansible controller 
 
-sudo apt update -y
+# sudo apt update -y
+# sudo apt-get install tree -y
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+# sudo apt install ansible -y
+# sudo apt install python3-pip -y
+# pip3 install awscli -y
+# pip3 install boto boto3 -y
+# python3 -m pip install --user --upgrade pip
+# python3 -m pip install ansible boto3 botocore
+# sudo apt-get upgrade -y
+
+sudo apt-get update -y
+
 sudo apt-get install tree -y
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
-sudo apt install python3-pip -y
-pip3 install awscli -y
-pip3 install boto boto3 -y
-python3 -m pip install --user --upgrade pip
-python3 -m pip install ansible boto3 botocore
-sudo apt-get upgrade -y
+
+sudo apt-get install software-properties-common
+
+sudo apt-add-repository ppa:ansible/ansible
+
+sudo apt-get update -y
+
+sudo apt-get install ansible

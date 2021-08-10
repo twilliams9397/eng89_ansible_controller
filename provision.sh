8ebfb76e081f2ae85abafd30a7df3cc50bb28198
twilliams9397 <twilliams@spartaglobal.com>
2021-08-10 11:06:19 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -23,9 +23,20 @@ sudo apt-get update -y
 
 sudo apt-get install ansible -y
 
+sudo rm -rf /etc/hosts
+
 sudo echo "
 [web]
 192.168.33.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant
 
 [db]
-192.168.33.11 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> /etc/hosts
+192.168.33.11 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant
+
+127.0.0.1	localhost
+127.0.1.1	vagrant.vm	vagrant
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+127.0.2.1 controller controller" >> /etc/hosts

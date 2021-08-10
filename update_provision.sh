f85485f0879f42ffe48942812fc100dcd06a5a11
twilliams9397 <twilliams@spartaglobal.com>
2021-08-10 13:18:39 +0100

    Update update_provision.sh

--- update_provision.sh
+++ update_provision.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update -y
 
-export "DB_HOST=mongodb://192.168.33.11:27017/posts" >> ~/.bashrc
+export "DB_HOST=ubuntu@192.168.33.11:27017/posts?authSource=admin" >> ~/.bashrc
 
 source ~/.bashrc
